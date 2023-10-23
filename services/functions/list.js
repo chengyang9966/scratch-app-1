edd41f751a82020cd550bbb676bd8cf70e5f49df
devchengyang <devchengyang@gmail.com>
2023-10-23 21:21:31 +0800

    fix query

--- services/functions/list.js
+++ services/functions/list.js
@@ -7,12 +7,13 @@ export const main = handler(async (event) => {
     // 'KeyConditionExpression' defines the condition for the query
     // - 'userId = :userId': only return items with matching 'userId'
     //   partition key
-    KeyConditionExpression: "userId = :userId, isActive = :isActive",
+    KeyConditionExpression: "userId = :userId",
+    FilterExpression: 'isActive = :isActive',
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be the id of the author
     ExpressionAttributeValues: {
       ":userId": event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      ":isActive":true
+      ':isActive': { BOOL: true }
     },
   };
 

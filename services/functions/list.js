75005131ac95223388585aeeec939779a0515180
devchengyang <devchengyang@gmail.com>
2023-10-23 21:36:54 +0800

    fix query add isActive

--- services/functions/list.js
+++ services/functions/list.js
@@ -7,16 +7,12 @@ export const main = handler(async (event) => {
     // 'KeyConditionExpression' defines the condition for the query
     // - 'userId = :userId': only return items with matching 'userId'
     //   partition key
-    KeyConditionExpression: "#userId = :userId",
-    FilterExpression: 'isActive = :isActive',
-    ExpressionAttributeNames: {
-      '#userid': 'userid',  // Replace 'userid' with your actual attribute name
-    },
+    KeyConditionExpression: "userId = :userId",
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be the id of the author
     ExpressionAttributeValues: {
       ":userId": event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      ':isActive': { BOOL: true }
+      ":isActive":true
     },
   };
 
